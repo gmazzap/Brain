e7088c5b73c1804e2d274ddafc2427f1927c0173
Giuseppe Mazzapica <giuseppe.mazzapica@gmail.com>
2014-02-28 07:11:57 +0100

    bad typo

--- boot.php
+++ boot.php
@@ -1,7 +1,7 @@
 <?php
 if ( ! class_exists( 'Brain\Container' ) ) require_once __DIR__ . '/vendor/autoload.php';
 
-if ( function_exists( 'after_setup_theme' ) ) {
+if ( function_exists( 'add_action' ) ) {
     add_action( 'after_setup_theme',
         function() {
         Brain\Container::boot( new \Pimple );
